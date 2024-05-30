e9db72c064c40c32d9af6c39578374200c585d70
Wasan <33808589+WasanYang@users.noreply.github.com>
2024-05-30 09:19:15 +0000

    Copilot third commit

---
+++ comments.js
@@ -0,0 +1,10 @@
+// Create web server
+const http = require('http');
+const port = 3000;
+const server = http.createServer((req, res) => {
+    res.writeHead(200, { 'Content-Type': 'application/json' });
+    res.write(JSON.stringify(skillsMember()));
+    res.end();
+});
+server.listen(port);
+console.log(`Server listening on port ${port}`);
